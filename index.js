bd470b4888cad709900844419ebcdfdc9bfe997f
Salomon Gutierrez <salomon19982009@hotmail.com>
2023-11-02 16:14:02 -0500

    Start

---
+++ index.js
@@ -0,0 +1,6 @@
+const titulo = document.getElementById("titulo")
+const tituloPrincipal = "Frutas"
+document.getElementById("titulo").innerHTML = tituloPrincipal
+console.log(titulo)
+const barras = document.querySelectorAll(".barras")
+console.log(barras)
